84674bcc54b5fba97c107e14ae9630a50baef44f
Mrowr Purr <mrowr.purr@gmail.com>
2025-03-26 01:35:17 -0700

    remove currently unused owning plugin name

--- SkyrimScripting.Services/include/SkyrimScripting/Services/ServiceInfo.h
+++ SkyrimScripting.Services/include/SkyrimScripting/Services/ServiceInfo.h
@@ -6,7 +6,6 @@ namespace SkyrimScripting::Services {
         const char* name;
         const char* description;
         void*       service_pointer;
-        const char* owning_plugin_name;
 
         template <typename T>
         T* service() {
