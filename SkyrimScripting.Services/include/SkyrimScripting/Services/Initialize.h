7b39fbd356b85d885c88cb077b69444c4f35162e
Mrowr Purr <mrowr.purr@gmail.com>
2025-03-27 16:10:17 -0700

    Make Initialize() header-only inline

--- SkyrimScripting.Services/include/SkyrimScripting/Services/Initialize.h
+++ SkyrimScripting.Services/include/SkyrimScripting/Services/Initialize.h
@@ -1,11 +1,36 @@
 #pragma once
 
+#include <SKSE/SKSE.h>
+
+#include <atomic>
+
+#include "IServicesService.h"
+
 namespace SkyrimScripting::Services {
+
+    namespace {
+        std::atomic<bool> _initialized = false;
+    }
+
     /**
      * The `SkyrimScripting::Services::Initialize` function **MUST** be called during the plugin's
      * initialization.
      *
      * > Must call this BEFORE the `kPostLoad` event.
      */
-    void Initialize();
+    inline void Initialize() {
+        if (_initialized.exchange(true)) return;
+
+        SKSE::GetMessagingInterface()->RegisterListener(
+            "SkyrimScripting.Services",
+            [](SKSE::MessagingInterface::Message* message) {
+                if (message->type == 69) {
+                    if (message->data != nullptr) {
+                        auto servicesService = static_cast<IServicesService*>(message->data);
+                        IServicesService::set(servicesService);
+                    }
+                }
+            }
+        );
+    }
 }
